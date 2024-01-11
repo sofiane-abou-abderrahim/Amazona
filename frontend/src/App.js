dc5ce601369ada03b5767d9ec0ecba73d580b15d
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2024-01-11 01:09:33 +0100

    rendered 4 products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,21 @@
+import data from './data';
+
 function App() {
   return (
     <div>
       <header>
         <a href="/">Amazona</a>
       </header>
-      <main>List products</main>
+      <main>
+        <h1>Featured Products</h1>
+        {data.products.map(product => (
+          <div>
+            <img src={product.image} alt={product.name} />
+            <p>{product.name}</p>
+            <p>{product.price}</p>
+          </div>
+        ))}
+      </main>
     </div>
   );
 }
