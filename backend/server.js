ec0dcdff4dace2c93b1fe497e80b9944d4df9fec
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2024-01-11 21:47:40 +0100

    defined port

--- backend/server.js
+++ backend/server.js
@@ -6,3 +6,8 @@ const app = express();
 app.get('/api/products', (req, res) => {
   res.send(data.products);
 });
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`serve at http://localhost:${port}`);
+});
