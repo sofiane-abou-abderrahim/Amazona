e56869b438e68dbb9fb0e618c85ac4d08d5f026e
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2024-01-11 01:14:32 +0100

    applied styles to the 4 products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,13 +8,17 @@ function App() {
       </header>
       <main>
         <h1>Featured Products</h1>
-        {data.products.map(product => (
-          <div key={product.slug}>
-            <img src={product.image} alt={product.name} />
-            <p>{product.name}</p>
-            <p>{product.price}</p>
-          </div>
-        ))}
+        <div className="products">
+          {data.products.map(product => (
+            <div className="product" key={product.slug}>
+              <img src={product.image} alt={product.name} />
+              <div className="product-info">
+                <p>{product.name}</p>
+                <p>{product.price}</p>
+              </div>
+            </div>
+          ))}
+        </div>
       </main>
     </div>
   );
