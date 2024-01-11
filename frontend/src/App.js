617447c9488669fff36a920dc9bfbd1c756fba0e
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2024-01-11 01:24:55 +0100

    added add to cart button + link to product page + styled price

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,10 +11,17 @@ function App() {
         <div className="products">
           {data.products.map(product => (
             <div className="product" key={product.slug}>
-              <img src={product.image} alt={product.name} />
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
               <div className="product-info">
-                <p>{product.name}</p>
-                <p>{product.price}</p>
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>
+                  <strong>${product.price}</strong>
+                </p>
+                <button>Add to cart</button>
               </div>
             </div>
           ))}
