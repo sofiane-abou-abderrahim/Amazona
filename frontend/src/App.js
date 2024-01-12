09bfa69d117562a092f77e5462db07fd9a45b0ba
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2024-01-12 22:00:56 +0100

    created & styled a footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ import { LinkContainer } from 'react-router-bootstrap';
 function App() {
   return (
     <BrowserRouter>
-      <div>
+      <div className="d-flex flex-column site-container">
         <header>
           <Navbar bg="dark" variant="dark">
             <Container>
@@ -26,6 +26,9 @@ function App() {
             </Routes>
           </Container>
         </main>
+        <footer>
+          <div className="text-center">All rights reserved</div>
+        </footer>
       </div>
     </BrowserRouter>
   );
