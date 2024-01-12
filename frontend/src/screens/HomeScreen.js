481fc23b4b214e64298240769461548d01e55ab1
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2024-01-12 23:13:39 +0100

    created product component in Product.js & returned jsx code inside Product function

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -5,6 +5,7 @@ import axios from 'axios';
 // import data from '../data';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Product from '../components/Product';
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -51,20 +52,7 @@ function HomeScreen() {
           <Row>
             {products.map(product => (
               <Col sm={6} md={4} lg={3} className="mb-3">
-                <div className="product" key={product.slug}>
-                  <Link to={`/product/${product.slug}`}>
-                    <img src={product.image} alt={product.name} />
-                  </Link>
-                  <div className="product-info">
-                    <Link to={`/product/${product.slug}`}>
-                      <p>{product.name}</p>
-                    </Link>
-                    <p>
-                      <strong>${product.price}</strong>
-                    </p>
-                    <button>Add to cart</button>
-                  </div>
-                </div>
+                <Product product={product}></Product>
               </Col>
             ))}
           </Row>
