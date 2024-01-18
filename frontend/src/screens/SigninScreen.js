cf254ad7bde4201203cb53ed34ac079115a95726
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2024-01-18 12:19:11 +0100

    defined Helmet to set the title of the page & h1 to set the heading

--- frontend/src/screens/SigninScreen.js
+++ frontend/src/screens/SigninScreen.js
@@ -1,3 +1,10 @@
 export default function SigninScreen() {
-  return <Container className="small-container"></Container>;
+  return (
+    <Container className="small-container">
+      <Helmet>
+        <title>Sign In</title>
+      </Helmet>
+      <h1 className="my-3">Sign In</h1>
+    </Container>
+  );
 }
