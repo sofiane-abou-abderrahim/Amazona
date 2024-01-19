fc2f7994552bb50cac550c46e19aa71b7fb4a6c2
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2024-01-19 17:06:14 +0100

    connected to mongodb atlas database in server.js

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,17 @@ import data from './data.js';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('connected to db');
+  })
+  .catch(err => {
+    console.log(err.message);
+  });
+
 const app = express();
 
 app.get('/api/products', (req, res) => {
