70305ed9f9f67f5e0116b157a4eb30ae04c03313
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2024-01-20 12:31:46 +0100

    defined seed router get api

--- backend/routes/seedRoutes.js
+++ backend/routes/seedRoutes.js
@@ -1,3 +1,13 @@
 import express from 'express';
+import Product from '../models/productModel';
+import data from '../data.js';
 
 const seedRouter = express.Router();
+
+seedRouter.get('/', async (req, res) => {
+  await Product.remove({});
+  const createdProducts = await Product.insertMany(data.products);
+  res.send({ createdProducts });
+});
+
+export default seedRouter;
