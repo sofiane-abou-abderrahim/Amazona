84ae2aad4ba06aba8f94663162c2a72d7e0f2063
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2024-01-22 12:47:37 +0100

    defined error handler for express in server.js

--- backend/server.js
+++ backend/server.js
@@ -22,6 +22,10 @@ app.use('/api/seed', seedRouter);
 
 app.use('/api/products', productRouter);
 
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`serve at http://localhost:${port}`);
