4110696179e59d082fb1764d05358af919f1c910
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2024-01-25 00:10:18 +0100

    prevented from accessing signin page while already signed in with useEffect

--- frontend/src/screens/SigninScreen.js
+++ frontend/src/screens/SigninScreen.js
@@ -4,7 +4,7 @@ import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import { Helmet } from 'react-helmet-async';
-import { useContext, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { Store } from '../Store';
 
 export default function SigninScreen() {
@@ -18,6 +18,8 @@ export default function SigninScreen() {
 
   const { state, dispatch: ctxDispatch } = useContext(Store);
 
+  const { userInfo } = state;
+
   const submitHandler = async e => {
     e.preventDefault();
     try {
@@ -28,12 +30,18 @@ export default function SigninScreen() {
 
       ctxDispatch({ type: 'USER_SIGNIN', payload: data });
       // console.log(data);
-      localStorage.setItem('userinfo', JSON.stringify(data));
+      localStorage.setItem('userInfo', JSON.stringify(data));
       navigate(redirect || '/');
     } catch (err) {
       alert('Invalid email or password');
     }
   };
+
+  useEffect(() => {
+    if (userInfo) {
+      navigate(redirect);
+    }
+  }, [navigate, redirect, userInfo]);
 
   return (
     <Container className="small-container">
