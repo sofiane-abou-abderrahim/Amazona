a7f71cccb9245c0a641080571f7dba0f6474f200
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2024-01-26 22:53:35 +0100

    made the shipping address content in a small container

--- frontend/src/screens/ShippingAddressScreen.js
+++ frontend/src/screens/ShippingAddressScreen.js
@@ -18,54 +18,56 @@ export default function ShippingAddressScreen() {
       <Helmet>
         <title>Shipping Address</title>
       </Helmet>
-      <h1 className="my-3">Shipping Address</h1>
-      <Form onSubmit={submitHandler}>
-        <Form.Group className="mb-3" controlId="fullName">
-          <Form.Label>Full Name</Form.Label>
-          <Form.Control
-            value={fullName}
-            onChange={e => setFullName(e.target.value)}
-            required
-          />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="address">
-          <Form.Label>Address</Form.Label>
-          <Form.Control
-            value={address}
-            onChange={e => setAddress(e.target.value)}
-            required
-          />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="city">
-          <Form.Label>City</Form.Label>
-          <Form.Control
-            value={city}
-            onChange={e => setCity(e.target.value)}
-            required
-          />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="postalCode">
-          <Form.Label>Postal Code</Form.Label>
-          <Form.Control
-            value={postalCode}
-            onChange={e => setPostalCode(e.target.value)}
-            required
-          />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="country">
-          <Form.Label>Country</Form.Label>
-          <Form.Control
-            value={country}
-            onChange={e => setCountry(e.target.value)}
-            required
-          />
-        </Form.Group>
-        <div className="mb-3">
-          <Button variant="primary" type="submit">
-            Continue
-          </Button>
-        </div>
-      </Form>
+      <div className="container small-container">
+        <h1 className="my-3">Shipping Address</h1>
+        <Form onSubmit={submitHandler}>
+          <Form.Group className="mb-3" controlId="fullName">
+            <Form.Label>Full Name</Form.Label>
+            <Form.Control
+              value={fullName}
+              onChange={e => setFullName(e.target.value)}
+              required
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="address">
+            <Form.Label>Address</Form.Label>
+            <Form.Control
+              value={address}
+              onChange={e => setAddress(e.target.value)}
+              required
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="city">
+            <Form.Label>City</Form.Label>
+            <Form.Control
+              value={city}
+              onChange={e => setCity(e.target.value)}
+              required
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="postalCode">
+            <Form.Label>Postal Code</Form.Label>
+            <Form.Control
+              value={postalCode}
+              onChange={e => setPostalCode(e.target.value)}
+              required
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="country">
+            <Form.Label>Country</Form.Label>
+            <Form.Control
+              value={country}
+              onChange={e => setCountry(e.target.value)}
+              required
+            />
+          </Form.Group>
+          <div className="mb-3">
+            <Button variant="primary" type="submit">
+              Continue
+            </Button>
+          </div>
+        </Form>
+      </div>
     </div>
   );
 }
