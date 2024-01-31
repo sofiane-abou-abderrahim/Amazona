65240716dfaa96bfd4935adccbebcde8768a5684
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2024-01-31 12:37:26 +0100

    added page title using Helmet & heading h1

--- frontend/src/screens/PaymentMethodScreen.js
+++ frontend/src/screens/PaymentMethodScreen.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Helmet } from 'react-helmet-async';
 import CheckoutSteps from '../components/CheckoutSteps';
 
 export default function PaymentMethodScreen() {
   return (
     <div>
       <CheckoutSteps step1 step2 step3></CheckoutSteps>
+      <div className="container small-container">
+        <Helmet>
+          <title>Payment Method</title>
+        </Helmet>
+        <h1 className="my-3">Payment Method</h1>
+      </div>
     </div>
   );
 }
