ff50fb996e0c5a810e73c51e7648bab411dbcc9b
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2024-02-01 20:34:40 +0100

    defined reducer

--- frontend/src/screens/PlaceOrderScreen.js
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -9,6 +9,19 @@ import ListGroup from 'react-bootstrap/ListGroup';
 import CheckoutSteps from '../components/CheckoutSteps';
 import { Store } from '../Store';
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'CREATE_REQUEST':
+      return { ...state, loading: true };
+    case 'CREATE_SUCCESS':
+      return { ...state, loading: false };
+    case 'CREATE_FAIL':
+      return { ...state, loading: false };
+    default:
+      return state;
+  }
+};
+
 export default function PlaceOrderScreen() {
   const navigate = useNavigate();
 
