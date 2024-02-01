52b11b63c371a6c3b4403d8b817253ca60b34b24
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2024-02-01 18:26:03 +0100

    got state & dispatch & userInfo from React context & state

--- frontend/src/screens/PlaceOrderScreen.js
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Helmet } from 'react-helmet-async';
+import { Link, useNavigate } from 'react-router-dom';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import ListGroup from 'react-bootstrap/ListGroup';
 import CheckoutSteps from '../components/CheckoutSteps';
+import { Store } from '../Store';
 
 export default function PlaceOrderScreen() {
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const { cart, userInfo } = state;
+
   return (
     <div>
       <CheckoutSteps step1 step2 step3 step4></CheckoutSteps>
