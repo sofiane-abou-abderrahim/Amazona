e612bb30025a1289dc7f87aa2a2f204ec2c53b90
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2024-02-01 18:21:01 +0100

    created row and columns for the page

--- frontend/src/screens/PlaceOrderScreen.js
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
 import CheckoutSteps from '../components/CheckoutSteps';
 
 export default function PlaceOrderScreen() {
@@ -10,6 +13,22 @@ export default function PlaceOrderScreen() {
         <title>Preview Order</title>
       </Helmet>
       <h1 className="my-3">Preview Order</h1>
+      <Row>
+        <Col md={8}>
+          <Card className="mb-3">
+            <Card.Body>
+              <Card.Title>Shipping</Card.Title>
+              <Card.Text>
+                <strong>Name:</strong> {cart.shippingAddress.fullName} <br />
+                <strong>Address:</strong> {cart.shippingAddress.address},
+                {cart.shippingAddress.city},{cart.shippingAddress.postalCode},
+                {cart.shippingAddress.country}
+              </Card.Text>
+              <Link to="/shipping">Edit</Link>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
     </div>
   );
 }
