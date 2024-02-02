fa9e5890462e4313f83eaea28df759d0c90bea46
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2024-02-02 17:04:11 +0100

    imported LoadingBox & MessageBox

--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
 
 export default function OrderScreen() {
   return loading ? (
