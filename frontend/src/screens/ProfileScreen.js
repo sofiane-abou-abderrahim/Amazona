cdc1ec1bb51755b11855e7ae344c96e49595697b
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2024-02-08 15:52:25 +0100

    created the rest of form inputs

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -24,6 +24,32 @@ export default function ProfileScreen() {
             required
           />
         </Form.Group>
+        <Form.Group className="mb-3" controlId="name">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            type="email"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+            required
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            onChange={e => setPassword(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="password">
+          <Form.Label>Confirm Password</Form.Label>
+          <Form.Control
+            type="password"
+            onChange={e => setConfirmPassword(e.target.value)}
+          />
+        </Form.Group>
+        <div className="mb-3">
+          <Button type="submit">Update</Button>
+        </div>
       </form>
     </div>
   );
