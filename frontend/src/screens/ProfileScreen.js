0a5ea1071fa4c6a505c6f3848066f184250f48c3
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2024-02-08 15:50:48 +0100

    created a form + set the first field & defined name field from useState

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -1,10 +1,13 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Helmet } from 'react-helmet-async';
+import Form from 'react-bootstrap/Form';
 import { Store } from '../Store';
 
 export default function ProfileScreen() {
   const { state, dispatch: ctxDispatch } = useContext(Store);
   const { userInfo } = state;
+
+  const [name, setName] = useState(userInfo.name);
 
   return (
     <div className="container small-container">
@@ -12,6 +15,16 @@ export default function ProfileScreen() {
         <title>User Profile</title>
       </Helmet>
       <h1 className="my-3">User Profile</h1>;
+      <form onSubmit={submitHandler}>
+        <Form.Group className="mb-3" controlId="name">
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            value={name}
+            onChange={e => setName(e.target.value)}
+            required
+          />
+        </Form.Group>
+      </form>
     </div>
   );
 }
