a84a47aea402071c80804fdb26e0fc488c2a9c17
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2024-02-08 15:47:13 +0100

    set page title + h1 heading

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -1,9 +1,17 @@
 import React, { useContext } from 'react';
+import { Helmet } from 'react-helmet-async';
 import { Store } from '../Store';
 
 export default function ProfileScreen() {
   const { state, dispatch: ctxDispatch } = useContext(Store);
   const { userInfo } = state;
 
-  return <div></div>;
+  return (
+    <div className="container small-container">
+      <Helmet>
+        <title>User Profile</title>
+      </Helmet>
+      <h1 className="my-3">User Profile</h1>;
+    </div>
+  );
 }
