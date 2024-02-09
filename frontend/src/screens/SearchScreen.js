50d4f3cd973ab6c581a7a7ef65f5a3e1f1b3ed0b
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2024-02-09 16:25:06 +0100

    defined search from react useLocation

--- frontend/src/screens/SearchScreen.js
+++ frontend/src/screens/SearchScreen.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 export default function SearchScreen() {
   const navigate = useNavigate();
+  const { search } = useLocation();
 
   return <div></div>;
 }
