a3fa63a2867bd0118e158e3e32d636268424e003
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2024-02-09 16:26:51 +0100

    retrieved the query string from URLSearchParams

--- frontend/src/screens/SearchScreen.js
+++ frontend/src/screens/SearchScreen.js
@@ -4,6 +4,7 @@ import { useLocation, useNavigate } from 'react-router-dom';
 export default function SearchScreen() {
   const navigate = useNavigate();
   const { search } = useLocation();
+  const sp = new URLSearchParams(search); // /search?category=Shirts
 
   return <div></div>;
 }
