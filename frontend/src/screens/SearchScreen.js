8720dba96bd2e9bef2bb9d2d1bd2af396d15230d
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2024-02-09 16:32:55 +0100

    defined useReducer & reducer

--- frontend/src/screens/SearchScreen.js
+++ frontend/src/screens/SearchScreen.js
@@ -1,5 +1,26 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'FETCH_REQUEST':
+      return { ...state, loading: true };
+    case 'FETCH_SUCCESS':
+      return {
+        ...state,
+        products: action.payload.products,
+        page: action.payload.page,
+        pages: action.payload.pages,
+        countProducts: action.payload.countProducts,
+        loading: false
+      };
+    case 'FETCH_FAIL':
+      return { ...state, loading: false, error: action.payload };
+
+    default:
+      return state;
+  }
+};
 
 export default function SearchScreen() {
   const navigate = useNavigate();
@@ -11,6 +32,12 @@ export default function SearchScreen() {
   const rating = sp.get('rating') || 'all';
   const order = sp.get('order') || 'newest';
   const page = sp.get('page') || 1;
+
+  const [{ loading, error, products, pages, countProducts }, dispatch] =
+    useReducer(reducer, {
+      loading: true,
+      error: ''
+    });
 
   return <div></div>;
 }
