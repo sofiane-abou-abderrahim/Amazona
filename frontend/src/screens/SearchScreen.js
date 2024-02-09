61e055ae27ed9b0bc6c92aef19c3a20e2dfc2ee9
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2024-02-09 16:29:01 +0100

    retrieved other filters from query string

--- frontend/src/screens/SearchScreen.js
+++ frontend/src/screens/SearchScreen.js
@@ -6,6 +6,11 @@ export default function SearchScreen() {
   const { search } = useLocation();
   const sp = new URLSearchParams(search); // /search?category=Shirts
   const category = sp.get('category') || 'all';
+  const query = sp.get('query') || 'all';
+  const price = sp.get('price') || 'all';
+  const rating = sp.get('rating') || 'all';
+  const order = sp.get('order') || 'newest';
+  const page = sp.get('page') || 1;
 
   return <div></div>;
 }
