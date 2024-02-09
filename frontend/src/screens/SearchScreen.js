17a4faafa1ac0b63f5faf67a9597a623941bca3b
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2024-02-09 16:24:09 +0100

    defined navigate from react useNavigate

--- frontend/src/screens/SearchScreen.js
+++ frontend/src/screens/SearchScreen.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function SearchScreen() {
+  const navigate = useNavigate();
+
   return <div></div>;
 }
