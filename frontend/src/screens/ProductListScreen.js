02a108b5cec61057ea98a68809a75e022984c4d9
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2024-02-11 18:24:17 +0100

    defined a reducer to manage state of fetching products from backend

--- frontend/src/screens/ProductListScreen.js
+++ frontend/src/screens/ProductListScreen.js
@@ -1,5 +1,25 @@
 import React from 'react';
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'FETCH_REQUEST':
+      return { ...state, loading: true };
+    case 'FETCH_SUCCESS':
+      return {
+        ...state,
+        products: action.payload.products,
+        page: action.payload.page,
+        pages: action.payload.pages,
+        loading: false
+      };
+    case 'FETCH_FAIL':
+      return { ...state, loading: false, error: action.payload };
+
+    default:
+      return state;
+  }
+};
+
 export default function ProductListScreen() {
   return <div></div>;
 }
