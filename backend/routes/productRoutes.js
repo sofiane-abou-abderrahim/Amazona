72f1174f9c0259301757b784cede91d59921e1c6
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2024-02-12 12:53:06 +0100

    defined post request to create products

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -10,6 +10,28 @@ productRouter.get('/', async (req, res) => {
   res.send(products);
 });
 
+productRouter.post(
+  '/',
+  isAuth,
+  isAdmin,
+  expressAsyncHandler(async (req, res) => {
+    const newProduct = new Product({
+      name: 'sample name ' + Date.now(),
+      slug: 'sample-name-' + Date.now(),
+      image: '/images/aisha-les-parfums-d-igor-600x600.webp',
+      price: 0,
+      category: 'sample category',
+      brand: 'sample brand',
+      countInStock: 0,
+      rating: 0,
+      numReviews: 0,
+      description: 'sample description'
+    });
+    const product = await newProduct.save();
+    res.send({ message: 'Product Created', product });
+  })
+);
+
 const PAGE_SIZE = 3;
 
 productRouter.get(
