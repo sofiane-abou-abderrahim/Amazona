6ea72f5955b0ca3454c70c8b60d9a34f79caebd5
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2024-02-12 15:29:50 +0100

    defined reducer to handle state of fetching data from backend

--- frontend/src/screens/ProductEditScreen.js
+++ frontend/src/screens/ProductEditScreen.js
@@ -1,6 +1,19 @@
 import React, { useContext } from 'react';
 import { Store } from '../Store';
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'FETCH_REQUEST':
+      return { ...state, loading: true };
+    case 'FETCH_SUCCESS':
+      return { ...state, loading: false };
+    case 'FETCH_FAIL':
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
 export default function ProductEditScreen() {
   const { state } = useContext(Store);
   const { userInfo } = state;
