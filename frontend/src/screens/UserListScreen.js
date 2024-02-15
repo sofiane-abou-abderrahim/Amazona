9c8bb38a9bd8a330506f5e585b342a75121247d8
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2024-02-15 22:37:34 +0100

    implemented useReducer to handle loading, error & users states

--- frontend/src/screens/UserListScreen.js
+++ frontend/src/screens/UserListScreen.js
@@ -1,5 +1,10 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 
 export default function UserListScreen() {
+  const [{ loading, error, users }, dispatch] = useReducer(reducer, {
+    loading: true,
+    error: ''
+  });
+
   return <div></div>;
 }
