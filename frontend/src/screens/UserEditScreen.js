9df387914daa400189352fe32046d7d7c4f99cac
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2024-02-15 23:02:16 +0100

    implemented reducer

--- frontend/src/screens/UserEditScreen.js
+++ frontend/src/screens/UserEditScreen.js
@@ -1,5 +1,24 @@
 import React from 'react';
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'FETCH_REQUEST':
+      return { ...state, loading: true };
+    case 'FETCH_SUCCESS':
+      return { ...state, loading: false };
+    case 'FETCH_FAIL':
+      return { ...state, loading: false, error: action.payload };
+    case 'UPDATE_REQUEST':
+      return { ...state, loadingUpdate: true };
+    case 'UPDATE_SUCCESS':
+      return { ...state, loadingUpdate: false };
+    case 'UPDATE_FAIL':
+      return { ...state, loadingUpdate: false };
+    default:
+      return state;
+  }
+};
+
 export default function UserEditScreen() {
   return <div></div>;
 }
