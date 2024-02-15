783ec308291a83985f5644161fa589c1d14d7618
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2024-02-15 20:58:03 +0100

    returned loadingDelete in the component

--- frontend/src/screens/OrderListScreen.js
+++ frontend/src/screens/OrderListScreen.js
@@ -98,6 +98,7 @@ export default function OrderListScreen() {
         <title>Orders</title>
       </Helmet>
       <h1>Orders</h1>
+      {loadingDelete && <LoadingBox></LoadingBox>}
       {loading ? (
         <LoadingBox></LoadingBox>
       ) : error ? (
