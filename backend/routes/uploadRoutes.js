5fa8513754a474ebd4367f09e41f03f86cef253c
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2024-02-15 12:08:10 +0100

    configured cloudinary

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -2,6 +2,12 @@ import express from 'express';
 import { v2 as cloudinary } from 'cloudinary';
 import streamifier from 'streamifier';
 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
 const uploadRouter = express.Router();
 
 uploadRouter.post('/', async (req, res) => {});
