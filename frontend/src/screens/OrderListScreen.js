eeb52020dddc50e99cfdf42772596efa5ab45fcd
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2024-02-15 14:50:33 +0100

    retrieved userInfo from context to authenticate the user when managing orders

--- frontend/src/screens/OrderListScreen.js
+++ frontend/src/screens/OrderListScreen.js
@@ -1,5 +1,9 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Store } from '../Store';
 
 export default function OrderListScreen() {
+  const { state } = useContext(Store);
+  const { userInfo } = state;
+
   return <div></div>;
 }
