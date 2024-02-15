8e8550783023697c7ea82d1dbc8cfce564b8cec9
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2024-02-15 22:38:58 +0100

    implemented reducer

--- frontend/src/screens/UserListScreen.js
+++ frontend/src/screens/UserListScreen.js
@@ -1,5 +1,23 @@
 import React, { useReducer } from 'react';
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'FETCH_REQUEST':
+      return { ...state, loading: true };
+    case 'FETCH_SUCCESS':
+      return {
+        ...state,
+        users: action.payload,
+        loading: false
+      };
+    case 'FETCH_FAIL':
+      return { ...state, loading: false, error: action.payload };
+
+    default:
+      return state;
+  }
+};
+
 export default function UserListScreen() {
   const [{ loading, error, users }, dispatch] = useReducer(reducer, {
     loading: true,
