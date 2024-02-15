997df8594325a27378d762dc8cb287f6c9d5e918
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2024-02-15 12:18:35 +0100

    created a stream upload function in the handler of this api & called it

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -19,7 +19,22 @@ uploadRouter.post(
   isAuth,
   isAdmin,
   upload.single('file'),
-  async (req, res) => {}
+  async (req, res) => {
+    const streamUpload = req => {
+      return new Promise((resolve, reject) => {
+        const stream = cloudinary.uploader.upload_stream((error, result) => {
+          if (result) {
+            resolve(result);
+          } else {
+            reject(error);
+          }
+        });
+        streamifier.createReadStream(req.file.buffer).pipe(stream);
+      });
+    };
+    const result = await streamUpload(req);
+    res.send(result);
+  }
 );
 
 export default uploadRouter;
