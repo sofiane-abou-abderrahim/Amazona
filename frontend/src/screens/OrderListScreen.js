13f58fbb86ac92fb457a8d10015cab7840648de9
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2024-02-15 14:53:39 +0100

    implemented useEffect to fetch data from api

--- frontend/src/screens/OrderListScreen.js
+++ frontend/src/screens/OrderListScreen.js
@@ -1,9 +1,29 @@
-import React, { useContext } from 'react';
+import axios from 'axios';
+import React, { useContext, useEffect } from 'react';
 import { Store } from '../Store';
+import { getError } from '../utils';
 
 export default function OrderListScreen() {
   const { state } = useContext(Store);
   const { userInfo } = state;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        dispatch({ type: 'FETCH_REQUEST' });
+        const { data } = await axios.get(`/api/orders`, {
+          headers: { Authorization: `Bearer ${userInfo.token}` }
+        });
+        dispatch({ type: 'FETCH_SUCCESS', payload: data });
+      } catch (err) {
+        dispatch({
+          type: 'FETCH_FAIL',
+          payload: getError(err)
+        });
+      }
+    };
+    fetchData();
+  }, [userInfo]);
 
   return <div></div>;
 }
