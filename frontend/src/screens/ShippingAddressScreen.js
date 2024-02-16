a21b76d47b32cdb6037b6c84f2287d4c6839bbfa
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2024-02-16 20:03:57 +0100

    created choose location on map button

--- frontend/src/screens/ShippingAddressScreen.js
+++ frontend/src/screens/ShippingAddressScreen.js
@@ -110,6 +110,26 @@ export default function ShippingAddressScreen() {
               required
             />
           </Form.Group>
+
+          <div className="mb-3">
+            <Button
+              id="chooseOnMap"
+              type="button"
+              variant="light"
+              onClick={() => navigate('/map')}
+            >
+              Choose Location On Map
+            </Button>
+            {shippingAddress.location && shippingAddress.location.lat ? (
+              <div>
+                LAT: {shippingAddress.location.lat}
+                LNG:{shippingAddress.location.lng}
+              </div>
+            ) : (
+              <div>No location</div>
+            )}
+          </div>
+
           <div className="mb-3">
             <Button variant="primary" type="submit">
               Continue
