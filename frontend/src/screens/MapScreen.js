b78dce9b7355913274d2d8e88fd22cc7de75ea32
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2024-02-16 20:36:36 +0100

    defined useEffect

--- frontend/src/screens/MapScreen.js
+++ frontend/src/screens/MapScreen.js
@@ -1,5 +1,14 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import { Store } from '../Store';
 
 export default function MapScreen() {
-  return <div className="full-box"></div>;
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+
+  useEffect(() => {
+    ctxDispatch({
+      type: 'SET_FULLBOX_ON'
+    });
+  }, [ctxDispatch]);
+
+  return <div className="full-box">123</div>;
 }
